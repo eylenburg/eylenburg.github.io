52ef17ca6da45db63b970a2aae918d891ef2e66f
user <user@localhost.localdomain>
2024-02-23 10:36:47 +0000

    various updates

---
+++ bottom.js
@@ -0,0 +1,28 @@
+document.write(`
+
+<footer>
+<hr style="width: 50%"><p style="text-align: center">Do you have any comments or corrections? <strong><a href="mailto:eylenburg@airmail.cc">Please drop me an e-mail</a></strong> or <strong><a href="https://github.com/eylenburg/eylenburg.github.io/issues" target="_blank">create an issue on Github</a></strong></p>
+
+
+<details style="text-align: center;">
+<summary>
+<span>Donations welcome! (click for details)</span></summary>
+<p>
+<strong>Monero:</strong> <a href="monero:87z2UAoivrhUXa52N8YniyQDUrMLAio1QdJtmZhark2K9EKRLQ5Deaba76tXXvRnFQ1kPXutZFENcDgk7VTkxd5C1PfNcWt">87z2UAoivrhUXa52N8YniyQDUrMLAio1QdJtmZhark2K9EKRLQ5Deaba76tXXvRnFQ1kPXutZFENcDgk7VTkxd5C1PfNcWt</a><br />
+<strong>Bitcoin:</strong> <a href="bitcoin:bc1qyy98j05a6kr99qmycn0n36mv9sveq5a3g6rnwe">bc1qyy98j05a6kr99qmycn0n36mv9sveq5a3g6rnwe</a><br />
+<strong>Bitcoin Cash:</strong> <a href="bitcoincash:qrucvz5mtwknr3mwn4r2chhl0eqgxumfmvg02skwjf">qrucvz5mtwknr3mwn4r2chhl0eqgxumfmvg02skwjf</a><br />
+<strong>Litecoin:</strong> <a href="litecoin:ltc1qqsfknhy626fu2n2s773e0qdjlt4ghurmf9kc75">ltc1qqsfknhy626fu2n2s773e0qdjlt4ghurmf9kc75</a><br />
+<strong>Kaspa:</strong> <a href="kaspa:qqg3wea6twrhs2ha878763fqepesxcw5qeeul9cwn4v5w0xt3undggeen33tw">qqg3wea6twrhs2ha878763fqepesxcw5qeeul9cwn4v5w0xt3undggeen33tw</a>
+</p>
+</details>
+<div style="display: block; text-align: center;">
+<div style="display: inline-block"><a href="monero:87z2UAoivrhUXa52N8YniyQDUrMLAio1QdJtmZhark2K9EKRLQ5Deaba76tXXvRnFQ1kPXutZFENcDgk7VTkxd5C1PfNcWt"><img src="pics/logos/other/crypto/xmr.svg" width="30" height="30" /></a></div>
+<div style="display: inline-block"><a href="bitcoin:bc1qyy98j05a6kr99qmycn0n36mv9sveq5a3g6rnwe"><img src="pics/logos/other/crypto/btc.svg" width="30" height="30" /></a></div>
+<div style="display: inline-block"><a href="bitcoincash:qrucvz5mtwknr3mwn4r2chhl0eqgxumfmvg02skwjf"><img src="pics/logos/other/crypto/bch.svg" width="30" height="30" /></a></div>
+<div style="display: inline-block"><a href="litecoin:ltc1qqsfknhy626fu2n2s773e0qdjlt4ghurmf9kc75"><img src="pics/logos/other/crypto/ltc.svg" width="30" height="30" /></a></div>
+<div style="display: inline-block"><a href="kaspa:qqg3wea6twrhs2ha878763fqepesxcw5qeeul9cwn4v5w0xt3undggeen33tw"><img src="pics/logos/other/crypto/kas.svg" width="30" height="30" /></a></div>
+</div>
+</footer>
+</body>
+
+`);
