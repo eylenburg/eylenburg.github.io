2943d0dbccfcea9eb871a5df9fbe25a3e94e23e1
eylenburg <84839316+eylenburg@users.noreply.github.com>
2024-03-19 15:22:31 +0000

    Add files via upload

--- top.js
+++ top.js
@@ -2,17 +2,17 @@ document.write(`
 
 <table class="menubar">
 <tr>
-<td><a href="os_comparison.htm">Compa&shy;rison of Ope&shy;ra&shy;ting Sys&shy;tems</a></td>
-<td><a href="os_familytree.htm">Fami&shy;ly Tree of Ope&shy;ra&shy;ting Sys&shy;tems</a></td>
-<td><a href="de_comparison.htm">Compa&shy;rison &amp; List of Desk&shy;top Envi&shy;ron&shy;ments</a></td>
-<td><a href="de_default.htm">Time&shy;line of De&shy;fault Desk&shy;top Envi&shy;ron&shy;ments</a></td>
-<td><a href="linux_comparison.htm">Compa&shy;rison of Li&shy;nux Dis&shy;tri&shy;bu&shy;tions</a></td>
-<td><a href="linux_distros_over_time.htm">Linux Dis&shy;tri&shy;bu&shy;tions over Time</a></td>
-<td><a href="android_comparison.htm">Compa&shy;rison of Andro&shy;id ROMs</a></td>
-<td><a href="browser_comparison.htm">Compa&shy;rison of Brow&shy;sers</a></td>
-<td><a href="browser_engines.htm">His&shy;tory of Brow&shy;ser En&shy;gines</a></td>
-<td><a href="im_comparison.htm">Compa&shy;rison of Ins&shy;tant Mes&shy;sen&shy;gers</a></td>
-<td><a href="cloud_comparison.htm">Compa&shy;rison of Cloud, Sync &amp; E&shy;mail pro&shy;viders</a></td>
+<td><a href="os_comparison.htm"><strong>Ope&shy;ra&shy;ting Sys&shy;tem</strong> Compa&shy;rison</a></td>
+<td><a href="os_familytree.htm"><strong>Ope&shy;ra&shy;ting Sys&shy;tem</strong> His&shy;tory &amp; Fami&shy;ly Tree</a></td>
+<td><a href="de_comparison.htm"><strong>Desk&shy;top Envi&shy;ron&shy;ment</strong> Compa&shy;rison &amp; List</a></td>
+<td><a href="de_default.htm">Time&shy;line of De&shy;fault <strong>Desk&shy;top Envi&shy;ron&shy;ments</strong></a></td>
+<td><a href="linux_comparison.htm"><strong>Li&shy;nux Dis&shy;tri&shy;bu&shy;tion</strong> Compa&shy;rison</a></td>
+<td><a href="linux_distros_over_time.htm"><strong>Linux Dis&shy;tri&shy;bu&shy;tions</strong> over Time</a></td>
+<td><a href="android_comparison.htm"><strong>Andro&shy;id ROM</strong> Compa&shy;rison</a></td>
+<td><a href="browser_comparison.htm"><strong>Web Brow&shy;ser</strong> Compa&shy;rison</a></td>
+<td><a href="browser_engines.htm"><strong>Brow&shy;ser En&shy;gine</strong> His&shy;tory</a></td>
+<td><a href="im_comparison.htm"><strong>Ins&shy;tant Mes&shy;sen&shy;ger</strong> Compa&shy;rison</a></td>
+<td><a href="cloud_comparison.htm"><strong>Cloud, Sync &amp; E&shy;mail Pro&shy;vider</strong> Compa&shy;rison</a></td>
 </tr>
 </table>
 
