3371ad87b0f40bc1a3a4296d42c437b86b9f0f76
eylenburg <84839316+eylenburg@users.noreply.github.com>
2024-03-19 17:51:29 +0000

    Add files via upload

--- top.js
+++ top.js
@@ -4,11 +4,10 @@ document.write(`
 <tr>
 <td><a href="os_comparison.htm"><strong>Ope&shy;ra&shy;ting Sys&shy;tem</strong> Compa&shy;rison</a></td>
 <td><a href="os_familytree.htm"><strong>Ope&shy;ra&shy;ting Sys&shy;tem</strong> His&shy;tory &amp; Fami&shy;ly Tree</a></td>
+<td><a href="android_comparison.htm"><strong>Andro&shy;id ROM</strong> Compa&shy;rison</a></td>
+<td><a href="linux_comparison.htm"><strong>Li&shy;nux Dis&shy;tri&shy;bu&shy;tion</strong> Compa&shy;rison &amp; Time&shy;line</a></td>
 <td><a href="de_comparison.htm"><strong>Desk&shy;top Envi&shy;ron&shy;ment</strong> Compa&shy;rison &amp; List</a></td>
 <td><a href="de_default.htm">Time&shy;line of De&shy;fault <strong>Desk&shy;top Envi&shy;ron&shy;ments</strong></a></td>
-<td><a href="linux_comparison.htm"><strong>Li&shy;nux Dis&shy;tri&shy;bu&shy;tion</strong> Compa&shy;rison</a></td>
-<td><a href="linux_distros_over_time.htm"><strong>Linux Dis&shy;tri&shy;bu&shy;tions</strong> over Time</a></td>
-<td><a href="android_comparison.htm"><strong>Andro&shy;id ROM</strong> Compa&shy;rison</a></td>
 <td><a href="browser_comparison.htm"><strong>Web Brow&shy;ser</strong> Compa&shy;rison</a></td>
 <td><a href="browser_engines.htm"><strong>Brow&shy;ser En&shy;gine</strong> His&shy;tory</a></td>
 <td><a href="im_comparison.htm"><strong>Ins&shy;tant Mes&shy;sen&shy;ger</strong> Compa&shy;rison</a></td>
