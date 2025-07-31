44a53767aaca3f15daf9b8df24a7b0f526f6d9c2
eylenburg <84839316+eylenburg@users.noreply.github.com>
2025-07-31 20:18:28 +0100

    CSS

--- top.js
+++ top.js
@@ -2,22 +2,22 @@ document.write(`
 
 <table class="menubar">
 <tr>
-<td><a href="os_comparison.htm">(1a)<strong> Ope&shy;ra&shy;ting Sys&shy;tems</strong> - Compa&shy;rison</a></td>
-<td><a href="os_familytree.htm">(1b)<strong> Ope&shy;ra&shy;ting Sys&shy;tems</strong> - His&shy;tory &amp; Fami&shy;ly Tree</a></td>
-<td><a href="android_comparison.htm">(2)<strong> Andro&shy;id "ROMs"</strong> - Compa&shy;rison</a></td>
-<td><a href="linux_comparison.htm">(3)<strong> Li&shy;nux Dis&shy;tri&shy;bu&shy;tions</strong> - Compa&shy;rison &amp; Time&shy;line</a></td>
-<td><a href="de_comparison.htm">(4a)<strong> Desk&shy;top Envi&shy;ron&shy;ments</strong> - Compa&shy;rison &amp; List</a></td>
-<td><a href="de_default.htm">(4b) <strong>Desk&shy;top Envi&shy;ron&shy;ments</strong> - De&shy;fault DE Time&shy;line</a></td>
-<td><a href="browser_comparison.htm">(5a) <strong>Web Brow&shy;sers</strong> - Compa&shy;rison</a></td>
-<td><a href="browser_engines.htm">(5b) <strong>Web Brow&shy;sers</strong> - En&shy;gine His&shy;tory</a></td>
-<td><a href="im_comparison.htm">(6) <strong>Ins&shy;tant Mes&shy;sen&shy;gers</strong> - Compa&shy;rison</a></td>
-<td><a href="cloud_comparison.htm">(7) <strong>Cloud, Sync &amp; E&shy;mail Pro&shy;viders</strong> - Compa&shy;rison</a></td>
-<td><a href="payments.htm">(8) <strong>Pay&shy;ment Me&shy;thods</strong> - Compa&shy;rison</a></td>
-<td><a href="excel.htm">(9) <strong>Spread&shy;sheet Pro&shy;grams</strong> - Compa&shy;rison</a></td>
-<td><a href="countries.htm">(10) <strong>Digi&shy;tal Free&shy;dom</strong> across the World</a></td>
+<td><a href="os_comparison.htm">(1a)<strong> Operating Systems</strong> - Comparison</a></td>
+<td><a href="os_familytree.htm">(1b)<strong> Operating Systems</strong> - History &amp; Family Tree</a></td>
+<td><a href="android_comparison.htm">(2)<strong> Android "ROMs"</strong> - Comparison</a></td>
+<td><a href="linux_comparison.htm">(3)<strong> Linux Distributions</strong> - Comparison &amp; Timeline</a></td>
+<td><a href="de_comparison.htm">(4a)<strong> Desktop Environments</strong> - Comparison &amp; List</a></td>
+<td><a href="de_default.htm">(4b) <strong>Desktop Environments</strong> - Default DE Timeline</a></td>
+<td><a href="browser_comparison.htm">(5a) <strong>Web Browsers</strong> - Comparison</a></td>
+<td><a href="browser_engines.htm">(5b) <strong>Web Browsers</strong> - Engine History</a></td>
+<td><a href="im_comparison.htm">(6) <strong>Instant Messengers</strong> - Comparison</a></td>
+<td><a href="cloud_comparison.htm">(7) <strong>Cloud, Sync &amp; Email Providers</strong> - Comparison</a></td>
+<td><a href="payments.htm">(8) <strong>Payment Methods</strong> - Comparison</a></td>
+<td><a href="excel.htm">(9) <strong>Spreadsheet Programs</strong> - Comparison</a></td>
+<td><a href="countries.htm">(10) <strong>Digital Freedom</strong> across the World</a></td>
 <td><a href="#"><strong>Other articles</strong></a>
     <div class="submenu">
-        <a href="altchans.htm"><strong>Altchans</strong> - image&shy;board acti&shy;vity be&shy;yond 4chan</a>
+        <a href="altchans.htm"><strong>Altchans</strong> - imageboard activity beyond 4chan</a>
     </div>
 </td>
 </tr>
