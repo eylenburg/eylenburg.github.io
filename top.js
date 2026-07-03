3a7853e4570a55af1ce5b3142c9cb67cf840907b
eylenburg <84839316+eylenburg@users.noreply.github.com>
2026-07-03 12:10:02 +0100

    Upgrade the AI /Search/Maps table to full article

--- top.js
+++ top.js
@@ -2,22 +2,22 @@ document.write(`
 
 <table class="menubar">
 <tr>
-<td><a href="os_comparison.htm">(1a)<strong> Operating Systems</strong> - Comparison</a></td>
-<td><a href="os_familytree.htm">(1b)<strong> Operating Systems</strong> - History &amp; Family Tree</a></td>
-<td><a href="android_comparison.htm">(2)<strong> Android Distributions</strong> - Comparison</a></td>
-<td><a href="linux_comparison.htm">(3)<strong> Linux Distributions</strong> - Comparison &amp; Timeline</a></td>
-<td><a href="de_comparison.htm">(4a)<strong> Desktop Environments</strong> - Comparison &amp; List</a></td>
-<td><a href="de_default.htm">(4b) <strong>Desktop Environments</strong> - Default DE Timeline</a></td>
-<td><a href="browser_comparison.htm">(5a) <strong>Web Browsers</strong> - Comparison</a></td>
-<td><a href="browser_engines.htm">(5b) <strong>Web Browsers</strong> - Engine History</a></td>
-<td><a href="im_comparison.htm">(6) <strong>Instant Messengers</strong> - Comparison</a></td>
-<td><a href="cloud_comparison.htm">(7) <strong>Cloud, Sync &amp; Email Providers</strong> - Comparison</a></td>
-<td><a href="payments.htm">(8) <strong>Payment Methods</strong> - Comparison</a></td>
-<td><a href="excel.htm">(9) <strong>Spreadsheet Programs</strong> - Comparison</a></td>
-<td><a href="countries.htm">(10) <strong>Digital Freedom</strong> across the World</a></td>
+<td><a href="countries.htm"><strong>Digital Freedom</strong> across the World</a></td>
+<td><a href="os_comparison.htm"><strong> Operating Systems</strong> - Comparison</a></td>
+<td><a href="os_familytree.htm"><strong> Operating Systems</strong> - History &amp; Family Tree</a></td>
+<td><a href="android_comparison.htm"><strong> Android Distributions</strong> - Comparison</a></td>
+<td><a href="linux_comparison.htm"><strong> Linux Distributions</strong> - Comparison &amp; Timeline</a></td>
+<td><a href="de_comparison.htm"><strong> Desktop Environments</strong> - Comparison &amp; List</a></td>
+<td><a href="de_default.htm"><strong>Desktop Environments</strong> - Default DE Timeline</a></td>
+<td><a href="browser_comparison.htm"><strong>Web Browsers</strong> - Comparison</a></td>
+<td><a href="browser_engines.htm"><strong>Web Browsers</strong> - Engine History</a></td>
+<td><a href="im_comparison.htm"><strong>Instant Messengers</strong> - Comparison</a></td>
+<td><a href="cloud_comparison.htm"><strong>Cloud, Sync &amp; Email Providers</strong> - Comparison</a></td>
+<td><a href="payments.htm"><strong>Payment Methods</strong> - Comparison</a></td>
+<td><a href="excel.htm"><strong>Spreadsheet Programs</strong> - Comparison</a></td>
+<td><a href="ai_search_maps.htm"><strong>Search, AI, Maps</strong> - Provider Overview</a></td>
 <td><a href="#"><strong>Other articles</strong></a>
     <div class="submenu">
-        <a href="ai_search_maps.htm">[2026] <strong>Search, AI, Maps</strong> - overview of providers</a>
         <a href="small_phones.htm">[2025] <strong>All phones are huge</strong> - a data-backed rant</a>
         <a href="altchans.htm">[2025] <strong>Altchans</strong> - imageboard activity beyond 4chan</a>
         <a href="linux_distrowatch.htm">[2023] <strong>Linux distro popularity</strong> (on Distrowatch)</a>
