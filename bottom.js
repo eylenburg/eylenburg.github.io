cfa025953fa2eac9522e3a039c7edaed9f302cf4
Nyte <39193783+codenyte@users.noreply.github.com>
2026-08-09 19:31:20 +0200

    Stylistic fixes

--- bottom.js
+++ bottom.js
@@ -1,7 +1,7 @@
 document.write(`
 
 <footer>
-<hr style="width: 50%"><p style="text-align: center">Do you have any comments or corrections? <strong><a href="https://github.com/eylenburg/eylenburg.github.io/issues" target="_blank">Please create an issue or pull request on Github</a></strong> or <strong><a href="mailto:eylenburg@airmail.cc">drop me an e-mail</a> (<a href="supporting/09E0AF0BDD9CB718B8B336DC62D5FF8DB2A98ACF.asc">&#x1f511; PGP Key</a>)</strong> <br />(NOTE: THERE IS CURRENTLY A PROBLEM WITH THE PROVIDER AND I CAN'T SEND/REPLY TO EMAILS. I CAN STILL READ THEM THOUGH.) <!--<sup class="tooltip">WARNING<span class="tooltiptext">Some unprofessional email providers will silently drop emails sent from my provider (so I won't even land in your spam folder, you will just never get my reply). These providers include Comcast, Vodafone/Arcor, United Internet (GMX, Mail.com, Web.de, 1&amp1, Freenet.de) and possibly more.</span></sup>--></p>
+<hr style="width: 50%"><p style="text-align: center">Do you have any comments or corrections? <strong><a href="https://github.com/eylenburg/eylenburg.github.io/issues" target="_blank">Please create an issue or pull request on GitHub</a></strong> or <strong><a href="mailto:eylenburg@airmail.cc">drop me an e-mail</a> (<a href="supporting/09E0AF0BDD9CB718B8B336DC62D5FF8DB2A98ACF.asc">&#x1f511; PGP Key</a>)</strong> <br />(NOTE: THERE IS CURRENTLY A PROBLEM WITH THE PROVIDER AND I CAN'T SEND/REPLY TO EMAILS. I CAN STILL READ THEM THOUGH.) <!--<sup class="tooltip">WARNING<span class="tooltiptext">Some unprofessional email providers will silently drop emails sent from my provider (so I won't even land in your spam folder, you will just never get my reply). These providers include Comcast, Vodafone/Arcor, United Internet (GMX, Mail.com, Web.de, 1&amp1, Freenet.de) and possibly more.</span></sup>--></p>
 
 <details style="text-align: center;">
 <summary>
@@ -20,13 +20,13 @@ document.write(`
 
 
 <div style="display: block; text-align: center;">
-<div style="display: inline-block"><a href="monero:87z2UAoivrhUXa52N8YniyQDUrMLAio1QdJtmZhark2K9EKRLQ5Deaba76tXXvRnFQ1kPXutZFENcDgk7VTkxd5C1PfNcWt"><img src="pics/logos/other/crypto/xmr.svg" width="30" height="30" /></a></div>
-<div style="display: inline-block"><a href="bitcoin:bc1qyy98j05a6kr99qmycn0n36mv9sveq5a3g6rnwe"><img src="pics/logos/other/crypto/btc.svg" width="30" height="30" /></a></div>
+<div style="display: inline-block"><a href="monero:87z2UAoivrhUXa52N8YniyQDUrMLAio1QdJtmZhark2K9EKRLQ5Deaba76tXXvRnFQ1kPXutZFENcDgk7VTkxd5C1PfNcWt"><img src="/pics/logos/other/crypto/xmr.svg" width="30" height="30" /></a></div>
+<div style="display: inline-block"><a href="bitcoin:bc1qyy98j05a6kr99qmycn0n36mv9sveq5a3g6rnwe"><img src="/pics/logos/other/crypto/btc.svg" width="30" height="30" /></a></div>
 <!--
-<div style="display: inline-block"><a href="bitcoincash:qrucvz5mtwknr3mwn4r2chhl0eqgxumfmvg02skwjf"><img src="pics/logos/other/crypto/bch.svg" width="30" height="30" /></a></div>
-<div style="display: inline-block"><a href="litecoin:ltc1qqsfknhy626fu2n2s773e0qdjlt4ghurmf9kc75"><img src="pics/logos/other/crypto/ltc.svg" width="30" height="30" /></a></div>
-<div style="display: inline-block"><a href="kaspa:qqg3wea6twrhs2ha878763fqepesxcw5qeeul9cwn4v5w0xt3undggeen33tw"><img src="pics/logos/other/crypto/kas.svg" width="30" height="30" /></a></div>-->
-<div style="display: inline-block"><a href="https://liberapay.com/eylenburg/"><img src="pics/logos/other/liberapay.svg" width="83" height="30" /></a></div>
+<div style="display: inline-block"><a href="bitcoincash:qrucvz5mtwknr3mwn4r2chhl0eqgxumfmvg02skwjf"><img src="/pics/logos/other/crypto/bch.svg" width="30" height="30" /></a></div>
+<div style="display: inline-block"><a href="litecoin:ltc1qqsfknhy626fu2n2s773e0qdjlt4ghurmf9kc75"><img src="/pics/logos/other/crypto/ltc.svg" width="30" height="30" /></a></div>
+<div style="display: inline-block"><a href="kaspa:qqg3wea6twrhs2ha878763fqepesxcw5qeeul9cwn4v5w0xt3undggeen33tw"><img src="/pics/logos/other/crypto/kas.svg" width="30" height="30" /></a></div>-->
+<div style="display: inline-block"><a href="https://liberapay.com/eylenburg/"><img src="/pics/logos/other/liberapay.svg" width="83" height="30" /></a></div>
 </div>
 </footer>
 
--- top.js
+++ top.js
@@ -2,26 +2,26 @@ document.write(`
 
 <table class="menubar">
 <tr>
-<td><a href="countries.htm"><strong>Digital Freedom</strong> across the World</a></td>
-<td><a href="os_comparison.htm"><strong> Operating Systems</strong> - Comparison</a></td>
-<td><a href="os_familytree.htm"><strong> Operating Systems</strong> - History &amp; Family Tree</a></td>
-<td><a href="android_comparison.htm"><strong> Android Distributions</strong> - Comparison</a></td>
-<td><a href="linux_comparison.htm"><strong> Linux Distributions</strong> - Comparison &amp; Timeline</a></td>
-<td><a href="de_comparison.htm"><strong> Desktop Environments</strong> - Comparison &amp; List</a></td>
-<td><a href="de_default.htm"><strong>Desktop Environments</strong> - Default DE Timeline</a></td>
-<td><a href="browser_comparison.htm"><strong>Web Browsers</strong> - Comparison</a></td>
-<td><a href="browser_engines.htm"><strong>Web Browsers</strong> - Engine History</a></td>
-<td><a href="im_comparison.htm"><strong>Instant Messengers</strong> - Comparison</a></td>
-<td><a href="cloud_comparison.htm"><strong>Cloud, Sync &amp; Email Providers</strong> - Comparison</a></td>
-<td><a href="payments.htm"><strong>Payment Methods</strong> - Comparison</a></td>
-<td><a href="excel.htm"><strong>Spreadsheet Programs</strong> - Comparison</a></td>
-<td><a href="ai_search_maps.htm"><strong>Search, AI, Maps</strong> - Provider Overview</a></td>
-<td><a href="#"><strong>Other articles</strong></a>
+<td><a href="/countries.htm"><strong>Digital Freedom</strong> across the World</a></td>
+<td><a href="/os_comparison.htm"><strong> Operating Systems</strong> - Comparison</a></td>
+<td><a href="/os_familytree.htm"><strong> Operating Systems</strong> - History &amp; Family Tree</a></td>
+<td><a href="/android_comparison.htm"><strong> Android Distributions</strong> - Comparison</a></td>
+<td><a href="/linux_comparison.htm"><strong> Linux Distributions</strong> - Comparison &amp; Timeline</a></td>
+<td><a href="/de_comparison.htm"><strong> Desktop Environments</strong> - Comparison &amp; List</a></td>
+<td><a href="/de_default.htm"><strong>Desktop Environments</strong> - Default DE Timeline</a></td>
+<td><a href="/browser_comparison.htm"><strong>Web Browsers</strong> - Comparison</a></td>
+<td><a href="/browser_engines.htm"><strong>Web Browsers</strong> - Engine History</a></td>
+<td><a href="/im_comparison.htm"><strong>Instant Messengers</strong> - Comparison</a></td>
+<td><a href="/cloud_comparison.htm"><strong>Cloud, Sync &amp; Email Providers</strong> - Comparison</a></td>
+<td><a href="/payments.htm"><strong>Payment Methods</strong> - Comparison</a></td>
+<td><a href="/excel.htm"><strong>Spreadsheet Programs</strong> - Comparison</a></td>
+<td><a href="/ai_search_maps.htm"><strong>Search, AI, Maps</strong> - Provider Overview</a></td>
+<td><a href="/#"><strong>Other articles</strong></a>
     <div class="submenu">
-        <a href="small_phones.htm">[2025] <strong>All phones are huge</strong> - a data-backed rant</a>
-        <a href="altchans.htm">[2025] <strong>Altchans</strong> - imageboard activity beyond 4chan</a>
-        <a href="linux_distrowatch.htm">[2023] <strong>Linux distro popularity</strong> (on Distrowatch)</a>
-        <a href="os_marketshare.htm">[2018] <strong>Historical OS Market Share</strong> since 1970</a>
+        <a href="/small_phones.htm">[2025] <strong>All phones are huge</strong> - a data-backed rant</a>
+        <a href="/altchans.htm">[2025] <strong>Altchans</strong> - imageboard activity beyond 4chan</a>
+        <a href="/linux_distrowatch.htm">[2023] <strong>Linux distro popularity</strong> (on Distrowatch)</a>
+        <a href="/os_marketshare.htm">[2018] <strong>Historical OS Market Share</strong> since 1970</a>
     </div>
 </td>
 </tr>
